eb1920c8f62928dbbb2b8f915dd97b60a06a2c97
Tanmay Somani <tanmaysomani2003@gmail.com>
2023-07-07 11:26:22 +0530

    Update privatize_repo.py

--- privatize_repo.py
+++ privatize_repo.py
@@ -1,21 +1,19 @@
 import os
 import subprocess
 
-# Access environment variable
 access_token = os.environ.get('ACCESS_TOKEN')
 
-# Configure Git user information
-subprocess.run(['git', 'config', '--global', 'user.name', 'Your Name'])
-subprocess.run(['git', 'config', '--global', 'user.email', 'your.email@example.com'])
+subprocess.run(['git', 'config', '--global', 'user.name', 'Tanmay Somani'])
+subprocess.run(['git', 'config', '--global', 'user.email', 'tanmaysomani2003@gmail.com'])
 
-# Initialize Git repository
+
 subprocess.run(['git', 'init'])
-subprocess.run(['git', 'remote', 'add', 'origin', 'https://github.com/your-username/your-repository.git'])
+subprocess.run(['git', 'remote', 'add', 'origin', 'https://github.com/Tanmay-Somani/your-repository.git'])
 subprocess.run(['git', 'fetch'])
 
-# Privatize the repository
 subprocess.run(['git', 'checkout', 'main'])
 subprocess.run(['git', 'update-ref', '-d', 'refs/heads/main'])
 subprocess.run(['git', 'checkout', '--orphan', 'main'])
 subprocess.run(['git', 'commit', '--allow-empty', '-m', 'Privatize repository'])
 subprocess.run(['git', 'push', '--force', 'origin', 'main'])
+
